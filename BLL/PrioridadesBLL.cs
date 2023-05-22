e4738410bcc23f9657e6fb616d60ae40a25af79d
Nathaly Goris Espino <nathalygorisespino@outlook.com>
2023-05-21 23:23:10 -0400

    first commit

---
+++ BLL/PrioridadesBLL.cs
@@ -0,0 +1,63 @@
+using System.Linq.Expressions;
+using Microsoft.EntityFrameworkCore;
+
+public class PrioridadesBLL{
+    private Context _context;
+
+    public PrioridadesBLL(Context context)
+    {
+        _context = context;
+    }
+
+    public bool Existe(int PrioridadId)
+    {
+        return _context.Prioridades.Any(o => o.PrioridadId == PrioridadId);
+    }
+
+    private bool Insertar(Prioridades 	Prioridades)
+    {
+        _context.Prioridades.Add(Prioridades);
+        return _context.SaveChanges() > 0;
+    }
+
+    private bool Modificar(Prioridades Prioridades)
+    {
+        var existe = _context.Prioridades.Find(Prioridades.PrioridadId);
+        if(existe != null)
+        {
+            _context.Entry(existe).CurrentValues.SetValues(Prioridades);
+            return _context.SaveChanges() > 0;
+        }
+
+        return false;
+    }
+
+    public bool Guardar(Prioridades Prioridades){
+        if(!Existe(Prioridades.PrioridadId))
+            return this.Insertar(Prioridades);
+        else
+            return this.Modificar(Prioridades);
+    }
+
+    public bool Eliminar(int PrioridadesId)
+    {
+        var eliminado  = _context.Prioridades.Where(o=> o.PrioridadId== PrioridadesId).SingleOrDefault();
+
+        if(eliminado!=null){
+            _context.Entry(eliminado).State = EntityState.Deleted;
+            return _context.SaveChanges() > 0;
+        }
+        return false;
+    }
+
+    public Prioridades? Buscar(int PrioridadId)
+    {
+        return _context.Prioridades.Where(o => o.PrioridadId == PrioridadId).AsNoTracking().SingleOrDefault();
+    }
+
+    public List<Prioridades> GetList(Expression<Func<Prioridades, bool>> criterio)
+    {
+        return _context.Prioridades.AsNoTracking().Where(criterio).ToList();
+    }
+
+}
---
+++ Models/Prioridades.cs
@@ -0,0 +1,11 @@
+using System.ComponentModel.DataAnnotations;
+
+public class Prioridades
+    {
+        [Key]
+        public int PrioridadId { get; set; }
+
+        [Required (ErrorMessage = "El codigo de la prioridad es obligatorio")]
+
+        public string? Codigo { get; set; }
+    }
